579e6ed063a8d9f827939e84af0f2ffa110a952e
Luis Mendez <luis.mendez@n5now.com>
2021-02-18 10:56:55 -0300

    p

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -17,15 +17,48 @@ namespace StarChart.Controllers
         {
             _context = context;
         }
-        [HttpGet("{id:int}")]
+        [HttpGet("{id:int}", Name ="GetById")]
         public IActionResult GetById (int id)
         {
-            var objectt = _context.CelestialObjects.FirstOrDefault(item => item.Id == id);
+            var celestialObject = _context.CelestialObjects.Find(id);
             
-            if (objectt.ToString()=="")
-                return Ok(objectt);
-            else
+            if (celestialObject==null)
                 return NotFound();
+            celestialObject.Satellites = _context.CelestialObjects.Where(e => e.OrbitedObjectId == id).ToList();
+                return Ok(celestialObject);
         }
+
+        [HttpGet("{name}", Name = "GetByName ")]
+        public IActionResult GetByName(string name)
+        {
+            var celestialObjects = _context.CelestialObjects.Where(e=> e.Name == name).ToList();
+
+            if (!celestialObjects.Any())
+                return NotFound();
+
+            foreach( var celestialObject in celestialObjects)
+            {
+                celestialObject.Satellites = _context.CelestialObjects.Where(e => e.OrbitedObjectId == celestialObject.Id).ToList();
+
+            }
+            return Ok(celestialObjects);
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var celestialObjects = _context.CelestialObjects.ToList();
+
+            if (!celestialObjects.Any())
+                return NotFound();
+
+            foreach (var celestialObject in celestialObjects)
+            {
+                celestialObject.Satellites = _context.CelestialObjects.Where(e => e.OrbitedObjectId == celestialObject.Id).ToList();
+
+            }
+            return Ok(celestialObjects);
+        }
+
     }
 }
